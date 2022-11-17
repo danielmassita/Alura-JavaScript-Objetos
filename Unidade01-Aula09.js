0eccba8b877c32d881761cbd833bc8c6ede8e66d
danielmassita <daniel.massita@gmail.com>
2022-11-17 12:21:13 -0300

    Fim da Unidade 01 Aula 10

--- Unidade01-Aula09.js
+++ Unidade01-Aula09.js
@@ -25,4 +25,14 @@ console.log(pessoa);
 
 /*
 E agora, você consegue incrementar ainda mais este objeto com informações de uma pessoa?
+
+10
+O que aprendemos?
+PRÓXIMA ATIVIDADE
+
+Nessa aula, você aprendeu:
+Que objeto é um tipo de dado que reflete uma abstração dos objetos da vida real;
+Que a estrutura de um objeto literal no JavaScript é composta por pares de chave: valor separados por vírgula, dentro de chaves { };
+Que para acessar dados em um objeto podemos usar as notações de ponto e de colchete;
+Que para adicionar e manipular dados em um objeto precisamos saber como acessar suas propriedades.
 */
