388f42430088d3338b333f3a136bacb5a5495055
danielmassita <daniel.massita@gmail.com>
2022-11-18 02:37:51 -0300

    Aula 03 - Propriedades de Prototype var.__proto__ ENTENDI NADA!

---
+++ Unidade04-Aula01.js
@@ -0,0 +1,10 @@
+/*
+01
+Projeto da aula anterior
+PRÓXIMA ATIVIDADE
+
+Você pode baixar o zip ou acessar o link do repositório da aula 3 no GitHub!
+https://github.com/alura-cursos/2259-js-objetos/archive/refs/heads/aula-3.zip
+https://github.com/alura-cursos/2259-js-objetos/tree/aula-3
+
+*/
